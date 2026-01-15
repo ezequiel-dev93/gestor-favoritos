8773635aa1fd38a38be57a8cdea2749ca526250a
Ezequiel <132577882+ezequiel-dev93@users.noreply.github.com>
2026-01-15 15:37:45 +0100

    feat: Add `AppLayout` component and update the root HTML element from `section` to `main`.

--- src/ui/layouts/AppLayout.tsx
+++ src/ui/layouts/AppLayout.tsx
@@ -5,8 +5,8 @@ import AsidePanel from "@/ui/components/AsidePanel";
 export function AppLayout() {
   const [asideOpen, setAsideOpen] = useState(true);
   return (
-    <main className="overflow-hidden text-zinc-800 dark:text-zinc-100 h-full w-auto">
+    <section className="overflow-hidden text-zinc-800 dark:text-zinc-100 h-full w-auto">
       <AsidePanel open={asideOpen} onOpenChange={setAsideOpen} />
-    </main>
+    </section>
   );
 }
